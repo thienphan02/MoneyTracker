c88665af182766aa63cb46a393bad26d81a51682
txphan <txphan@cs.olemiss.edu>
2024-01-28 12:05:52 -0600

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from  "styled-components"
+import bg from "./img/1_Z1FpdzDRmqVZQUN2O00hQA.png"
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from "./Components/Navigation/Navigation";
+import React, { useMemo, useState } from "react";
 
 function App() {
+  const [active, setActive] = React.useState(1)
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
+        <MainLayout>
+          <Navigation active={active} setActive={setActive} />
+          <main>
+            <h1>Hello</h1>
+          </main>
+        </MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+height: 100vh;
+background-image: url(${props => props.bg});
+position: relative;
+main{
+  flex: 1;
+  background: rgba(252, 246, 249, 0.78);
+  border: 3px solid #FFFFFF;
+  backdrop-filter: blur(4.5px);
+  border-radius: 32px;
+  overflow-x: hidden;
+  &::-webkit-scrollbar{
+    width: 0;
+  }
+}
+`;
 
 export default App;
